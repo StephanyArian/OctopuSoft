e87293c6fd7f048cfc2a82c78f3bec3e6935e2d0
YasminYanaYujra <dollyyasminyana@gmail.com>
2026-04-06 19:16:27 -0400

    Actualizacion de la tabla user con campos de role y status(T1),Crear índice único sobre el campo correo (T2),Creacion del modelo/entidad de Usuario (T3)

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,46 +2,34 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
-    use HasFactory, Notifiable;
+    use Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
+    // T3: campos que se pueden llenar masivamente
     protected $fillable = [
         'name',
         'email',
         'password',
+        'role',
+        'status',
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
+    // Campos que NUNCA se envían al frontend
     protected $hidden = [
         'password',
         'remember_token',
     ];
 
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
+    // Conversión automática de tipos
     protected function casts(): array
     {
         return [
             'email_verified_at' => 'datetime',
-            'password' => 'hashed',
+            'password'          => 'hashed', // T6: bcrypt automático
         ];
     }
 }
