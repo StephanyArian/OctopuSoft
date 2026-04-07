3a303487786a83b0c87d25a6373f9113d515804f
CynthiaChambi <cynthiabaltazar55@gamil.com>
2026-04-07 12:46:00 -0400

    T1 diseño de pantallas y flujo de recuperación de contraseña

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,19 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
+Route::get('/forgot-password', [PasswordResetLinkController::class, 'create'])
+     ->name('password.request');
 
+Route::post('/forgot-password', [PasswordResetLinkController::class, 'store'])
+     ->name('password.email');
+// Si usas Laravel Breeze o Fortify, la ruta ya existe automáticamente.
+// Si la manejas manualmente:
+Route::get('/reset-password/{token}', function ($token) {
+    return view('auth.RecupCont', ['token' => $token]);
+})->name('password.reset');
+
+Route::post('/reset-password', [NewPasswordController::class, 'store'])
+    ->name('password.update');
+    });
+  
 require __DIR__.'/auth.php';
