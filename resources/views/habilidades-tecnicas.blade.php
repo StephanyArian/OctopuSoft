5cdf7514431d20bb53f2bd787bfc364e26006c86
YasminYanaYujra <dollyyasminyana@gmail.com>
2026-05-10 19:26:38 -0400

    Cambio de icono de habilidades tecnicas

--- resources/views/habilidades-tecnicas.blade.php
+++ resources/views/habilidades-tecnicas.blade.php
@@ -253,7 +253,6 @@
                             @endif
 
                             {{-- Contador + historial --}}
-                            {{-- Contador + historial --}}
                             <div style="display:flex;align-items:center;gap:10px;margin-top:28px;flex-wrap:wrap;">
                                 @if($total > 0)
                                     <div class="historial-divider" style="flex:1;margin:0;">
@@ -312,15 +311,18 @@
 
                                         <div class="skill-actions">
                                             <a href="{{ route('skills.tecnicas') }}?edit={{ $skill->id }}" class="btn-sm">
-                                                <svg width="13" height="13" viewBox="0 0 13 13" fill="none">
-                                                    <path d="M9.5 1.5l2 2-7 7H2.5v-2l7-7z" stroke="currentColor" stroke-width="1.3" stroke-linecap="round"/>
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14"
+                                                    fill="currentColor" viewBox="0 0 16 16">
+                                                    <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325"/>
                                                 </svg>
                                                 Editar
                                             </a>
                                             <button class="btn-sm danger"
                                                     onclick="openDeleteModal('{{ $skill->id }}', '{{ addslashes($skill->name) }}')">
-                                                <svg width="13" height="13" viewBox="0 0 13 13" fill="none">
-                                                    <path d="M2 3.5h9M5 3.5V2.5h3v1M4.5 3.5l.5 7M8.5 3.5l-.5 7" stroke="currentColor" stroke-width="1.3" stroke-linecap="round"/>
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14"
+                                                     fill="currentColor" viewBox="0 0 16 16">
+                                                    <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"/>
+                                                    <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z"/>
                                                 </svg>
                                                 Eliminar
                                             </button>
