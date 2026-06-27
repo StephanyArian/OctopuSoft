e2c7da290a753c51f0776e804aadaf77c938cc0c
Maria Ramirez <ramirezsaravia89@gmail.com>
2026-06-27 19:11:39 -0400

    Mejora de mensaje Gmail2

---
+++ resources/views/vendor/mail/html/header.blade.php
@@ -0,0 +1,9 @@
+<tr>
+<td class="header">
+<a href="{{ config('app.url') }}" style="display: inline-block; text-decoration: none;">
+    <span style="font-size: 22px; font-weight: 700; color: #07866e; letter-spacing: 1px;">
+        
+    </span>
+</a>
+</td>
+</tr>
