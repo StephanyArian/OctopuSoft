887f099defdeb6bf8cecd5b8f667e91b753643f7
Maria Ramirez <ramirezsaravia89@gmail.com>
2026-06-27 19:15:49 -0400

    Boddy de correo1

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -2,7 +2,7 @@
 <td class="header">
 <a href="{{ config('app.url') }}" style="display: inline-block; text-decoration: none;">
     <span style="font-size: 22px; font-weight: 700; color: #07866e; letter-spacing: 1px;">
-        
+        OctopuSoft
     </span>
 </a>
 </td>
